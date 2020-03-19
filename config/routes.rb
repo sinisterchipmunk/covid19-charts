f17805cfa8d1a98b6449704d52a6eb26e1c5b500
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2020-03-19 13:58:42 -0400

    root to countries index

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: 'countries#heatmap'
+  root to: 'countries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
